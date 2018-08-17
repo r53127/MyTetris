1d30270c12220ef005fa2996e30f55910bb2fcc0
r53127 <rnkn@163.com>
2018-08-17 20:56:25 +0800

    update 20180817

--- TetrisWindow.py
+++ TetrisWindow.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication ,QMainWindow
 from PyQt5.QtGui import QPixmap,QPainter,QBitmap , QIcon , QCursor , QImageReader , QPainterPath , QPen , QImage
-from PyQt5.QtCore import Qt , QRect
+from PyQt5.QtCore import Qt , QRect ,QPoint
 from LayerClass import LayerClass
 
 class TetrisWindow(QMainWindow):
@@ -13,16 +13,30 @@ class TetrisWindow(QMainWindow):
             LayerClass(40, 32, 334, 279, img, 7),
             LayerClass(40, 343, 334, 279, img, 7),
             LayerClass(414, 32, 334, 590, img, 7),  # 游戏窗口，小方块为32*32像素，边框宽度为7，游戏窗口为16：9, 宽=32*10+14，高=18*32+14
-            LayerClass(788, 32, 334, 200, img, 7),
-            LayerClass(788, 405, 334, 200, img, 7)
-        ]
+            LayerClass(788, 32, 334, 124, img, 7),
+            LayerClass(788, 188, 176, 148, img, 7),
+            LayerClass(964, 188, 158, 148, img, 7),
+            LayerClass(788, 368, 334, 124, img, 7),
+            LayerClass(788, 524, 334, 98, img, 7),
+         ]
+        self.img_position={
+            (820, 536):QImage("Graphics/game/logo.png"),
+            (60, 52): QImage("Graphics/game/db.png"),
+            (60,363):QImage("Graphics/game/world.png"),
+            (820,64): QImage("Graphics/game/start.png"),
+            (970, 64): QImage("Graphics/game/exit.png"),
+            (1000, 208): QImage("Graphics/game/level.png"),
+            (808, 378): QImage("Graphics/game/score.png"),
+            (808, 418): QImage("Graphics/game/rmline.png")
+        }
+
 
 
     def initUI(self):
         self.setWindowTitle('多多大战俄罗斯方块')
         self.setWindowIcon(QIcon('Graphics\windows\windows.png'))
         self.pix = QBitmap("Graphics/Backgroud/mask3.png")
-        self.resize(self.pix.size())
+        self.resize(self.pix.size())#蒙板的尺寸为1162*654
         self.setMask(self.pix)
         self.setWindowOpacity(1)  # 设置透明度
         self.show()
@@ -51,9 +65,11 @@ class TetrisWindow(QMainWindow):
     def paintEvent(self, QPaintEvent):
         try:
             painter = QPainter(self)
-            painter.drawPixmap(0, 0, self.pix.width(), self.pix.height(), QPixmap("Graphics/Backgroud/010.jpg"))
+            painter.drawPixmap(0, 0, self.pix.width(), self.pix.height(), QPixmap("Graphics/Backgroud/screen1.jpg"))
             for layer in self.layers:
                 layer.CreateLayer(painter)
+            for img_pos in self.img_position:
+                painter.drawImage(QPoint(img_pos[0],img_pos[1]), self.img_position[img_pos])
 
         except BaseException as e:
             print('错误是:',e)
