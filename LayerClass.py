724eba09df4735aeb0c9fbde8ae5d968f70c28e8
r53127 <rnkn@163.com>
2018-08-21 19:51:02 +0800

    update 20180821

--- LayerClass.py
+++ LayerClass.py
@@ -1,10 +1,10 @@
-from PyQt5.QtCore import QRect , QPoint
+from PyQt5.QtCore import QRect, QPoint
 from PyQt5.QtGui import QImage
 
 
 class CONST():
-    SIZE = 7  # 边框切边宽度为7个像素
-    PADDING = 16  # 图片显示内边距为16个像素
+    SIZE = 7  # pic cornor width : 7 pixel
+    PADDING = 16  # pic padding : 16 pixel
     FrameImg = "Graphics/windows/windows.png"
     LogoImg = "Graphics/game/logo.png"
     DBImg = "Graphics/game/db.png"
@@ -17,7 +17,7 @@ class CONST():
 
 
 class LayerClass():
-    def __init__(self, x, y, w, h):  # x,y,w,h分别为图层的起始坐标（x,y)和大小（width,height),FrameImg为准备切片的图片名称
+    def __init__(self, x, y, w, h):  # Top-left coordinate and size of the layer is （x,y)  and （width,height)
         self.x = x
         self.y = y
         self.w = w
@@ -25,23 +25,26 @@ class LayerClass():
         self.imgW = QImage(CONST.FrameImg).width()
         self.imgH = QImage(CONST.FrameImg).height()
 
-    def CreateLayer(self, painter):
+    def createlayer(self, painter):
         # 左上
         painter.drawImage(QRect(self.x, self.y, CONST.SIZE, CONST.SIZE), QImage(CONST.FrameImg),
                           QRect(0, 0, CONST.SIZE, CONST.SIZE))
         # 中上
-        painter.drawImage(QRect(self.x + CONST.SIZE, self.y, self.w - 2 * CONST.SIZE, CONST.SIZE), QImage(CONST.FrameImg),
+        painter.drawImage(QRect(self.x + CONST.SIZE, self.y, self.w - 2 * CONST.SIZE, CONST.SIZE),
+                          QImage(CONST.FrameImg),
                           QRect(CONST.SIZE, 0, self.imgW - 2 * CONST.SIZE, CONST.SIZE))
         # 右上
         painter.drawImage(QRect(self.x + self.w - CONST.SIZE, self.y, CONST.SIZE, CONST.SIZE), QImage(CONST.FrameImg),
                           QRect(self.imgW - CONST.SIZE, 0, CONST.SIZE, CONST.SIZE))
         # 左中
-        painter.drawImage(QRect(self.x, self.y + CONST.SIZE, CONST.SIZE, self.h - 2 * CONST.SIZE), QImage(CONST.FrameImg),
+        painter.drawImage(QRect(self.x, self.y + CONST.SIZE, CONST.SIZE, self.h - 2 * CONST.SIZE),
+                          QImage(CONST.FrameImg),
                           QRect(0, CONST.SIZE, CONST.SIZE, self.imgH - 2 * CONST.SIZE))
         # 中中
-        painter.drawImage(QRect(self.x + CONST.SIZE, self.y + CONST.SIZE, self.w - 2 * CONST.SIZE, self.h - 2 * CONST.SIZE),
-                          QImage(CONST.FrameImg),
-                          QRect(CONST.SIZE, CONST.SIZE, self.imgW - 2 * CONST.SIZE, self.imgH - 2 * CONST.SIZE))
+        painter.drawImage(
+            QRect(self.x + CONST.SIZE, self.y + CONST.SIZE, self.w - 2 * CONST.SIZE, self.h - 2 * CONST.SIZE),
+            QImage(CONST.FrameImg),
+            QRect(CONST.SIZE, CONST.SIZE, self.imgW - 2 * CONST.SIZE, self.imgH - 2 * CONST.SIZE))
         # 右中
         painter.drawImage(QRect(self.x + self.w - CONST.SIZE, self.y + CONST.SIZE, CONST.SIZE, self.h - 2 * CONST.SIZE),
                           QImage(CONST.FrameImg),
@@ -51,82 +54,81 @@ class LayerClass():
                           QRect(0, self.imgH - CONST.SIZE, CONST.SIZE, CONST.SIZE))
         # 中下
         painter.drawImage(QRect(self.x + CONST.SIZE, self.y + self.h - CONST.SIZE, self.w - 2 * CONST.SIZE, CONST.SIZE),
-                          QImage(CONST.FrameImg), QRect(CONST.SIZE, self.imgH - CONST.SIZE, self.imgW - 2 * CONST.SIZE, CONST.SIZE))
+                          QImage(CONST.FrameImg),
+                          QRect(CONST.SIZE, self.imgH - CONST.SIZE, self.imgW - 2 * CONST.SIZE, CONST.SIZE))
         # 右下
-        painter.drawImage(QRect(self.x + self.w - CONST.SIZE, self.y + self.h - CONST.SIZE, CONST.SIZE, CONST.SIZE), QImage(
-            CONST.FrameImg),
+        painter.drawImage(QRect(self.x + self.w - CONST.SIZE, self.y + self.h - CONST.SIZE, CONST.SIZE, CONST.SIZE),
+                          QImage(CONST.FrameImg),
                           QRect(self.imgW - CONST.SIZE, self.imgH - CONST.SIZE, CONST.SIZE, CONST.SIZE))
 
-class DBLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
-        painter.drawImage(QPoint(self.x+CONST.PADDING,self.y+CONST.PADDING),QImage(CONST.DBImg))
+class DBLayer(LayerClass):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
+
+    def paint(self, painter):
+        self.createlayer(painter)
+        painter.drawImage(QPoint(self.x + CONST.PADDING, self.y + CONST.PADDING), QImage(CONST.DBImg))
+
 
 class WorldLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
-        painter.drawImage(QPoint(self.x+CONST.PADDING,self.y+CONST.PADDING),QImage(CONST.WorldImg))
+    def paint(self, painter):
+        self.createlayer(painter)
+        painter.drawImage(QPoint(self.x + CONST.PADDING, self.y + CONST.PADDING), QImage(CONST.WorldImg))
+
 
 class GameLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
+    def paint(self, painter):
+        self.createlayer(painter)
+
+
 
 class ButtonLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
-        painter.drawImage(QPoint(self.x+CONST.PADDING,self.y+CONST.PADDING),QImage(CONST.StartImg))
+    def paint(self, painter):
+        self.createlayer(painter)
+        painter.drawImage(QPoint(self.x + CONST.PADDING + 20, self.y + CONST.PADDING + 20), QImage(CONST.StartImg))
+
 
 class NextLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
+    def paint(self, painter):
+        self.createlayer(painter)
 
-class PointLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
-
-    def paint(self,painter):
-        self.CreateLayer(painter)
 
 class LevelLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
-        painter.drawImage(QPoint(self.x+CONST.PADDING,self.y+CONST.PADDING),QImage(CONST.LevelImg))
+    def paint(self, painter):
+        self.createlayer(painter)
+        painter.drawImage(QPoint(self.x + CONST.PADDING, self.y + CONST.PADDING), QImage(CONST.LevelImg))
+
+
+class PointLayer(LayerClass):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
+
+    def paint(self, painter):
+        self.createlayer(painter)
+        painter.drawImage(QPoint(self.x + CONST.PADDING, self.y + CONST.PADDING), QImage(CONST.ScoreImg))
+
 
 class AboutLayer(LayerClass):
-    def __init__(self,x,y,w,h):
-        super().__init__(x,y,w,h)
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
 
-    def paint(self,painter):
-        self.CreateLayer(painter)
-        painter.drawImage(QPoint(self.x+CONST.PADDING,self.y+CONST.PADDING),QImage(CONST.LogoImg))
-
-
-
-
-
-
-
-
-
-
-
-
+    def paint(self, painter):
+        self.createlayer(painter)
+        painter.drawImage(QPoint(self.x + CONST.PADDING + 10, self.y + CONST.PADDING), QImage(CONST.LogoImg))
 
--- TetrisWindow.py
+++ TetrisWindow.py
@@ -1,8 +1,11 @@
 import sys
-from PyQt5.QtWidgets import QApplication ,QMainWindow
-from PyQt5.QtGui import QPixmap,QPainter,QBitmap , QIcon , QCursor , QImageReader , QPainterPath , QPen , QImage
-from PyQt5.QtCore import Qt , QRect ,QPoint
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QPainter, QBitmap, QCursor, QIcon
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
 from LayerClass import *
+
 
 class TetrisWindow(QMainWindow):
     def __init__(self):
@@ -12,11 +15,11 @@ class TetrisWindow(QMainWindow):
         self.layers=[
             DBLayer(40, 32, 334, 279),
             WorldLayer(40, 343, 334, 279),
-            GameLayer(414, 32, 334, 590),  # 游戏窗口，小方块为32*32像素，边框宽度为7，游戏窗口为16：9, 宽=32*10+14，高=18*32+14
+            GameLayer(414, 32, 334, 590),  # game main window，small rect size is 32*32，game window width:height ratio is 16：9, width=32*10+14，height=18*32+14
             ButtonLayer(788, 32, 334, 124),
             NextLayer(788, 188, 176, 148),
-            PointLayer(964, 188, 158, 148),
-            LevelLayer(788, 368, 334, 124),
+            LevelLayer(964, 188, 158, 148),
+            PointLayer(788, 368, 334, 124),
             AboutLayer(788, 524, 334, 98),
          ]
 
@@ -26,9 +29,9 @@ class TetrisWindow(QMainWindow):
         self.setWindowTitle('多多大战俄罗斯方块')
         self.setWindowIcon(QIcon('Graphics\windows\windows.png'))
         self.pix = QBitmap("Graphics/Backgroud/mask3.png")
-        self.resize(self.pix.size())#蒙板的尺寸为1162*654
+        self.resize(self.pix.size())#Masking panel pic size：1162*654
         self.setMask(self.pix)
-        self.setWindowOpacity(1)  # 设置透明度
+        self.setWindowOpacity(1)  # set transparency
         self.show()
 
     def mousePressEvent(self, event):
@@ -44,7 +47,7 @@ class TetrisWindow(QMainWindow):
 
     def mouseMoveEvent(self, QMouseEvent):
         if Qt.LeftButton and self.m_drag:
-            # 当左键移动窗体修改偏移值
+            # when left button moved, modify window offset value
             self.move(QMouseEvent.globalPos() - self.m_DragPosition)
             QMouseEvent.accept()
 
@@ -59,7 +62,7 @@ class TetrisWindow(QMainWindow):
             for layer in self.layers:
                 layer.paint(painter)
         except BaseException as e:
-            print('错误是:',e)
+            print('Error is :',e)
 
 
 if __name__ == '__main__':
